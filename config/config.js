8f0c04feab5bc25cc3d06297460cb1fab4aa01e0
chandrainf <surel.chandrapratama@gmail.com>
2021-06-03 02:15:14 +0700

    update

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "SDJF0W850FJ5LDFJ4095809DFJG045F",
-  mongoUri: process.env.MONGODB_URI || "mongodb+srv://sysadmin:YBc6bStzgNYMRgss@cluster0.sgrvx.gcp.mongodb.net/miniERP?retryWrites=true&w=majority"
+  mongoUri: process.env.MONGODB_URI || "mongodb+srv://sysadmin:YBc6bStzgNYMRgss@cluster0.sgrvx.gcp.mongodb.net/mernsocial?retryWrites=true&w=majority"
     //process.env.MONGO_HOST ||
     //'mongodb://' + (process.env.IP || 'localhost') + ':' +
     //(process.env.MONGO_PORT || '27017') +
