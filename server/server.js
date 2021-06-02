78ce6b04e54aeaf68885450bdefee294b50304fb
chandrainf <surel.chandrapratama@gmail.com>
2021-06-03 04:27:28 +0700

    update

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose'
 
 // Connection URL
 mongoose.Promise = global.Promise
-mongoose.connect(config.mongoUri)
+mongoose.connect(config.mongoUri, {autoIndex: false})
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${mongoUri}`)
 })
@@ -15,12 +15,3 @@ app.listen(config.port, (err) => {
   }
   console.info('Server started on port %s.', config.port)
 })
-
-
-//if (process.env.NODE_ENV === 'production') {
-  //app.use(express.static('client/build/'));
-
-  //app.get('*', (req, res) => {
-   // res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
-  //});
-//}
