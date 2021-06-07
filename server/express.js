503d598f4084c967f82abbc2ac01be1db9fe7618
chandrainf <surel.chandrapratama@gmail.com>
2021-06-07 11:34:20 +0700

    update

--- server/express.js
+++ server/express.js
@@ -97,12 +97,6 @@ app.use((err, req, res, next) => {
   }
 })
 
-if (process.env.NODE_ENV === 'PRODUCTION') {
-  app.use(express.static(path.join(__dirname, '../dist/')))
 
-  app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, '../dist/index.html'))
-  })
-}
 
 export default app
--- server/server.js
+++ server/server.js
@@ -21,3 +21,11 @@ app.listen(config.port, (err) => {
   }
   console.info('Server started on port %s.', config.port)
 })
+
+if (process.env.NODE_ENV === 'PRODUCTION') {
+  app.use(express.static(path.join(__dirname, '../dist/')))
+
+  app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, '../dist/index.html'))
+  })
+}
